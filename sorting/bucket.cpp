3087a4ecc20558107f4f4aa4a95ff2cfe2e891a2
kartikgarg093 <kartikgarg093@gmail.com>
2026-05-11 16:06:52 +0530

    Sorting

--- sorting/bucket.cpp
+++ sorting/bucket.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main() {
-  int n,i;
+  int n,i,j;
   cout<<"Enter size of array:";
   cin>>n;
   int a[n];
   cout<<"Enter Elements of array:";
   for(i=0;i<n;i++)
   {
-    cin>>a[n];
+    cin>>a[i];
   }
   int max=a[0];
   for(i=1;i<n;i++)
@@ -26,6 +26,43 @@ int main() {
     int d=a[i]/10;
     b[d][c[d]]=a[i];
     c[d]++;
-}
+  }
+  for(i=0;i<(max/10+1);i++)
+  {
+    int h=c[i];
+    for(j=1;j<h;j++)
+    {
+      int temp=b[i][j];
+      for(int k=j-1;k>-1;k--)
+      {
+        if (b[i][k]>temp)
+        {
+          b[i][k+1]=b[i][k];
+          b[i][k]=temp;
+        }
+        else
+        {
+          break;
+        }
+      }
+    }
+  }
+  int index=0;
+
+  for(i=0;i<(max/10+1);i++)
+  {
+    int h=c[i];
+    for(j=0;j<h;j++)
+    {
+      a[index]=b[i][j];
+      index++;
+    }
+  }
+
+  cout<<"Sorted Array is:";
+ for(i=0;i<n;i++)
+ {
+   cout<<a[i]<<" ";
+ }
  return 0;
 }
